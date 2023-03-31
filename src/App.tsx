7987bdaae05c9904c513364a540cddd6e1627b16
Ilya Gamyrkin <igamyrkin@vtb.ru>
2023-03-31 14:54:18 +0300

    feat: Добавлен redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,79 +1,21 @@
-import { useCallback, useEffect, useRef, useState } from 'react';
+import { useEffect } from 'react';
 import './App.css';
-import data from './assets/x5.json';
-
-import ReactFlow, {
-  Connection,
-  Edge,
-  ReactFlowInstance,
-  ReactFlowProvider,
-  addEdge,
-  useEdgesState,
-  useNodesState,
-} from 'reactflow';
 import 'reactflow/dist/style.css';
-import { Items, Kind } from './types/item';
-
-interface CustomNode extends Node {}
-const test: Node;
-
-const initialNodes: Node[] = [
-  {
-    id: '1',
-    type: 'input',
-    data: { label: 'item' },
-    position: { x: 500, y: 5 },
-  },
-  {
-    id: '2',
-    type: 'input',
-    data: { label: 'item2' },
-    position: { x: 700, y: 5 },
-  },
-  {
-    id: '3',
-    type: 'default',
-    data: { label: 'group1' },
-    position: { x: 600, y: 100 },
-  },
-  {
-    id: '4',
-    type: 'output',
-    data: { label: 'cluster1' },
-    position: { x: 600, y: 200 },
-  },
-];
+import { useDispatch } from 'react-redux';
+import { AppDispatch } from './app/store';
+import { fetchItemsThunk } from './slices/items';
+import Tree from './components/Tree';
 
 function App() {
-  const reactFlowWrapper = useRef<HTMLDivElement>(null);
-  useEffect(() => {
-    const items = (data as Items).filter((item) => item.kind === Kind.ITEM)
-  }, []);
+  const dispatch = useDispatch<AppDispatch>();
 
-  const [nodes, setNodes, onNodesChange] = useNodesState([]);
-  const [edges, setEdges, onEdgesChange] = useEdgesState([]);
-  const [reactFlowInstance, setReactFlowInstance] = useState<ReactFlowInstance<any, Edge>>();
-  const onConnect = useCallback((params: Connection) => setEdges((eds) => addEdge(params, eds)), [setEdges]);
+  useEffect(() => {
+    dispatch(fetchItemsThunk());
+  }, [dispatch]);
+
   return (
     <div className="container">
-      <ReactFlowProvider>
-        <div className="reactflow-wrapper" ref={reactFlowWrapper}>
-          <ReactFlow
-            nodes={nodes}
-            edges={edges}
-            onNodesChange={onNodesChange}
-            onEdgesChange={onEdgesChange}
-            onConnect={onConnect}
-            onInit={setReactFlowInstance}
-            // onDrop={onDrop}
-            // onDragOver={onDragOver}
-            fitView
-          >
-            {/* <Controls /> */}
-          </ReactFlow>
-        </div>
-        {/* <Sidebar /> */}
-      </ReactFlowProvider>
+      <Tree />
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,17 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App';
-// import { store } from './app/store';
+import { store } from './app/store';
 import './index.css';
 
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const container = document.getElementById('root')!;
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    {/* <Provider store={store}> */}
+    <Provider store={store}>
       <App />
-    {/* </Provider> */}
+    </Provider>
   </React.StrictMode>,
 );
