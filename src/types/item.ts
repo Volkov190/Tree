ac1c73fef5adc21049cdeca7bf5fb003d7d3e96f
Volkov Fedor <f.volkov10@gmail.com>
2023-03-31 15:11:22 +0300

    feat: дополнены типы

--- src/types/item.ts
+++ src/types/item.ts
@@ -25,4 +25,46 @@ export type Item =
       name: string;
     };
 
+interface ClusterItem {
+  kind: Kind.CLUSTER;
+  uuid: string;
+  name: string;
+}
+
+interface GroupItem {
+  kind: Kind.GROUP;
+  uuid: string;
+  clusterUuid: string;
+  name: string;
+}
+
+interface ProductItem {
+  kind: Kind.ITEM;
+  uuid: string;
+  groupUuid: string | null;
+  important: boolean;
+  name: string;
+  description: string;
+}
+
+interface GroupedProductItem extends ProductItem {
+  groupUuid: string;
+}
+
+export const isCluster = (item: Item): item is ClusterItem => {
+  return item.kind === Kind.CLUSTER;
+};
+
+export const isGroup = (item: Item): item is GroupItem => {
+  return item.kind === Kind.GROUP;
+};
+
+export const isProduct = (item: Item): item is ProductItem => {
+  return item.kind === Kind.ITEM;
+};
+
+export const isGroupedProduct = (item: Item): item is GroupedProductItem => {
+  return isProduct(item) && !!item.groupUuid;
+};
+
 export type Items = Item[];
