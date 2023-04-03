a1b724d5a32648c6115384f56216ba029054a690
Ilya Gamyrkin <igamyrkin@vtb.ru>
2023-04-03 04:24:33 +0300

    feat: pages try 2

--- src/assets/icons/index.ts
+++
@@ -1,4 +0,0 @@
-export { ReactComponent as BackIcon } from './BackIcon.svg';
-export { ReactComponent as CloseOutline } from './CloseOutline.svg';
-export { ReactComponent as DeleteOutline } from './DeleteOutline.svg';
-export { ReactComponent as SaveOutline } from './SaveOutline.svg';
